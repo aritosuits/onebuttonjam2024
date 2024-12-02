a5cd0b289544d4827e93256a0f22ae814da1940b
Arito <aritosuits@gmail.com>
2024-12-02 07:59:20 -0500

    Fix for lose screen

--- state.lose.lua
+++ state.lose.lua
@@ -6,7 +6,7 @@ lose.fade = true
 function lose.enter()
 	lose.lock = 120
 	music(-1)
-	scroll.init(lose, 14, 'employee #' .. (flr(rnd(1000)) + 1000) .. '|terminated after|brutal encounter|with ' .. stats.killed_by .. '||employee earned|$' .. score.points .. ' in bonuses|' .. (score.points > 0 and 'but due to regulatory|mandate, only $2 will|be paid out|' or '') .. '|   :*#  *@ * %$(   @*|laura please send|in an intern to|fix:this pr1nt#r|  :   ::  3 *!@ **@||press 🅾️ to re-apply')
+	scroll.init(lose, 14, 'employee #' .. (flr(rnd(1000)) + 1000) .. '|terminated after|brutal encounter|with ' .. stats.killed_by .. '||||||employee earned|$' .. score.points .. ' in bonuses|' .. (score.points > 0 and 'but due to regulatory|mandate, only $2 will|be paid out|' or '') .. '|   :*#  *@ * %$(   @*|laura please send|in an intern to|fix:this pr1nt#r|  :   ::  3 *!@ **@||press 🅾️ to re-apply')
 end
 
 function lose.update(dt)
@@ -16,6 +16,9 @@ end
 
 function lose.draw()
 	scroll.draw()
+	if lose.line >= 7 then
+		spr(103, 40, 70, 3, 2)
+	end
 end
 
 function lose.press() end
